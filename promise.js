b9ab4e41207907df9ebf5424833309e9b6a0a064
khunzbear <khun.suwan16@gmail.com>
2024-04-19 03:02:09 +0700

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,21 +1,24 @@
-//Example promise
+const url = "kunzfile1";
+const connect = true;
 
-const url = "kunzfile1"
-const connect = true
-
-function downloading(url){
-
+function downloading(url) {
     return new Promise((resolve, reject) => {
-        
-        if (connect) {
 
-            resolve(`Downloaded`)
-            
-        }
-        else{
-
-            reject("Failed to download")
-        }
-
-    })
+        console.log("Start Dowloading...")
+        setTimeout(() => {
+            if (connect) {
+                resolve(`Downloaded ${url}`); // Then
+            } else {
+                reject(`Failed to download ${url}`); // Catch
+            }
+        }, 2000);
+    });
 }
+
+downloading(url)
+    .then((result) => {
+        console.log(result); // Print the result if download is successful
+    })
+    .catch((error) => {
+        console.error(error); // Print the error if download fails
+    });
